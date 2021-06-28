2a02db3a9653e2f1f27a48e9d870cf2c06ac53f6
genedeh <genesis.enedeh@gmail.com>
2021-06-28 17:41:16 +0100

    configured the admin.py

--- paragon_game/game/admin.py
+++ paragon_game/game/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# headers and titles
+admin.site.site_header = 'PARAGON ADMIN PANEL'
+admin.site.site_title = 'PARAGON GAME SITE ADMIN'
+admin.site.index_title = 'GAME ADMINISTRATION'
