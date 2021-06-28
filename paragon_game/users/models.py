bbc7ca47a06a6e136e2be6d25bed5db6e8654cd1
genedeh <genesis.enedeh@gmail.com>
2021-06-28 18:21:20 +0100

    made the first model called  Profile

--- paragon_game/users/models.py
+++ paragon_game/users/models.py
@@ -1,5 +1,26 @@
-from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class User(AbstractUser):
-    pass
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_address = models.EmailField()
+    bio = models.TextField(max_length=500, blank=True, default="")
+    location = models.CharField(max_length=30, help_text="Make sure it is a valid country or city")
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user}"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
