fedaac3cc8a226baac5cd287969cae2072bbfed3
genedeh <genesis.enedeh@gmail.com>
2021-06-28 19:48:51 +0100

    fixed a bug in admin.py in line 9

--- paragon_game/game/admin.py
+++ paragon_game/game/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from .models import Avatar, Bloodline
+
+class AvatarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bloodline', 'email_address', 'location')
 
 # Register your models here.
-
+admin.site.register(Avatar)
+admin.site.register(Bloodline)
 # headers and titles
 admin.site.site_header = 'PARAGON ADMIN PANEL'
 admin.site.site_title = 'PARAGON GAME SITE ADMIN'
--- paragon_game/game/models.py
+++ paragon_game/game/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Avatar(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    image = models.ImageField()
+    power = models.CharField(max_length=359)
+    bloodline = models.ForeignKey('Bloodline', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Bloodline(models.Model):
+    name = models.CharField(max_length=159, unique=True)
+    power_level = models.FloatField(unique="True")
+    symbol = models.ImageField()
+
+    def __str__(self):
+        return f"{self.name}"
