56e47300f7d6ae071ff8fc3e528d754b22e7b394
genedeh <genesis.enedeh@gmail.com>
2021-06-28 18:40:44 +0100

    reconfigured admin.py for display

--- paragon_game/users/admin.py
+++ paragon_game/users/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Profile
 
+
 # Register your models here.
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'birth_date', 'email_address', 'location')
+
+
+admin.site.register(Profile, ProfileAdmin)
