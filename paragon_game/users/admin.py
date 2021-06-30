e085f1bc2cb760dc97b7661f8cf3f63427f561c0
genedeh <genesis.enedeh@gmail.com>
2021-06-30 16:45:40 +0100

    removed anything  related to auth_user

--- paragon_game/users/admin.py
+++ paragon_game/users/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Player
+from .models import  Player
 
 
 # Register your models here.
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'birth_date', 'email_address', 'location')
-
 
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ("username", "avatar", "profile")
+    list_display = ("username", "avatar", "birth_date", "location")
 
-
-admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Player, PlayerAdmin)
--- paragon_game/users/models.py
+++ paragon_game/users/models.py
@@ -1,38 +1,15 @@
-from django import forms
 from django.db import models
-from django.contrib.auth.models import User
 from game.models import Avatar
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Player(models.Model):
+    username = models.CharField(max_length=299, unique=True)
+    password = models.CharField(max_length=20, help_text="Must have up to 8 characters", unique=True)
     email_address = models.EmailField()
     bio = models.TextField(max_length=500, blank=True, default="")
     location = models.CharField(max_length=30, help_text="Make sure it is a valid country or city")
     birth_date = models.DateField(null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.user}"
-
-
-class Player(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
-    password = models.CharField(max_length=20, help_text="Must have upto 8 characters")
-    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
     avatar = models.ForeignKey(Avatar, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.username}"
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
