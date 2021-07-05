997ebeb3a6a078370ff4a2ff675e622ec66010dd
Genedeh <genesis.enedeh@ggmail.com>
2021-07-05 19:40:30 +0100

    added a model called "Group" and created avatars and bloodlines

--- paragon_game/users/admin.py
+++ paragon_game/users/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
-from .models import Player
+from .models import Player, Group
 
 
 # Register your models here.
 
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ("username", "avatar", "birth_date", "location")
-#
-#
-# class GroupAdmin(admin.ModelAdmin):
-#     list_display = ("name", "created", "updated")
-#
-#
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "created", "updated")
+
+
 # class MemberAdmin(admin.ModelAdmin):
 #     list_display = ("name", "group_name")
 
 
 admin.site.register(Player, PlayerAdmin)
-# admin.site.register(Group, GroupAdmin)
-# admin.site.register(Member)
+admin.site.register(Group, GroupAdmin)
+# admin.site.register(Member, MemberAdmin)
