4530ad9c4175780292d764fd65124b64b3dbf453
Genedeh <genesis.enedeh@ggmail.com>
2021-07-10 00:33:10 +0100

    some design changes

--- paragon_game/game/views.py
+++ paragon_game/game/views.py
@@ -4,7 +4,14 @@ from .models import Avatar
 
 # Create your views here.
 def home_view(request):
-    return render(request, "home.html")
+    avatar = Avatar.objects.filter(name__contains="")
+    dis_avatar = avatar.order_by("bio")[2]
+    avatar2 = Avatar.objects.filter(name__contains="h")
+    dis_avatar2 = avatar2.order_by("bio")[1]
+    avatar3 = Avatar.objects.filter(name__contains="sh")
+    dis_avatar3 = avatar3.order_by("bio")[0]
+
+    return render(request, "home.html", {"dis_ava": dis_avatar, "dis_ava2":dis_avatar2, "dis_ava3":dis_avatar3})
 
 
 def avatar_view(request):
