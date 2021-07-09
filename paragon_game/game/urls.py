9ddd685cc2fe7511245be2281f2850f2cc120cd6
Genedeh <genesis.enedeh@ggmail.com>
2021-07-09 18:43:46 +0100

    created a new page calle avatars

--- paragon_game/game/urls.py
+++ paragon_game/game/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, avatar_view
 
 app_name = "game"
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('avatars', avatar_view, name='avatars')
 ]
--- paragon_game/game/views.py
+++ paragon_game/game/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Avatar
 
 
 # Create your views here.
 def home_view(request):
     return render(request, "home.html")
+
+
+def avatar_view(request):
+    avatar = Avatar.objects.all
+    return render(request, "Avatars.html", {"avatar": avatar})
