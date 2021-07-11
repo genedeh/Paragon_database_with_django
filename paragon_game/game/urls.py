7590d348a9e59230089a44126b533af5f220b6dd
Genedeh <genesis.enedeh@ggmail.com>
2021-07-11 09:19:26 +0100

    I create a bloodline html and view

--- paragon_game/game/urls.py
+++ paragon_game/game/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home_view, avatar_view
+from .views import home_view, avatar_view, bloodline_view
 
 app_name = "game"
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('avatars', avatar_view, name='avatars')
+    path('avatars', avatar_view, name='avatars'),
+    path('bloodlines', bloodline_view, name='bloodlines')
 ]
--- paragon_game/game/views.py
+++ paragon_game/game/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Avatar
+from .models import Avatar, Bloodline
 from users.models import Player
 
 
@@ -10,9 +10,15 @@ def num_of_tables_in_model(model):
 
 
 def home_view(request):
-    return render(request, "home.html", {"avatar_num": num_of_tables_in_model(Avatar), "player_num": num_of_tables_in_model(Player)})
+    return render(request, "home.html",
+                  {"avatar_num": num_of_tables_in_model(Avatar), "player_num": num_of_tables_in_model(Player)})
 
 
 def avatar_view(request):
     avatar = Avatar.objects.all
     return render(request, "Avatars.html", {"avatar": avatar})
+
+
+def bloodline_view(request):
+    bloodline = Bloodline.objects.all
+    return render(request, "Bloodlines.html", {"bloodline": bloodline})
