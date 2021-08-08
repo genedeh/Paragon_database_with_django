d6746bd9633c70f9b691e934473f7af7f2e91fbf
KidChaos <genesis.enedeh@gmail.com>
2021-08-08 22:22:53 +0100

    finished sign up form and design

--- paragon_game/users/forms.py
+++ paragon_game/users/forms.py
@@ -1,13 +1,17 @@
 from django import forms
 from django.forms import ModelForm
-
 from .models import Player
 
 
 class PlayerSigninForm(ModelForm):
     bio = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 10}))
     password = forms.CharField(widget=forms.PasswordInput)
-
+    birth_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     class Meta:
         model = Player
         fields = '__all__'
+        error_messages = {
+            'password': {
+                'max_length': "Password must not pass 20 characters",
+            },
+        }
