31d53034957787dbbbc1821c4018c9701ec572ac
KidChaos <genesis.enedeh@gmail.com>
2021-08-09 23:48:08 +0100

    created url pattern for user view

--- paragon_game/users/urls.py
+++ paragon_game/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import sign_in_view
+from .views import sign_in_view, UserView
 
 app_name = "users"
 
 urlpatterns = [
     path('signin', sign_in_view, name='Signin'),
+    path('Player/<pk>/', UserView.as_view(), name='player')
 ]
