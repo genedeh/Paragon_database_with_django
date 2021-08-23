222885ac82cbced53c9a322edd63f6319a6a519b
KidChaos <genesis.enedeh@gmail.com>
2021-08-23 22:56:26 +0100

    complete sign in form  and put in url to user page using slug

--- paragon_game/users/forms.py
+++ paragon_game/users/forms.py
@@ -4,9 +4,13 @@ from .models import Player
 
 
 class PlayerSigninForm(ModelForm):
-    bio = forms.CharField(widget=forms.Textarea(attrs={'cols': 70, 'rows': 7}))
-    password = forms.CharField(widget=forms.PasswordInput)
+    bio = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': 70, 'rows': 7, 'placeholder': 'Write about yourself...'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Username...'}))
+    email_address = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Enter Email...'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password...'}))
     birth_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+
     class Meta:
         model = Player
-        fields = '__all__'
+        fields = ['username', 'password', 'bio', 'email_address', 'location', 'birth_date', 'avatar']
--- paragon_game/users/urls.py
+++ paragon_game/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import sign_in_view, UserView
+from .views import sign_in_view, PlayerView
 
 app_name = "users"
 
 urlpatterns = [
     path('signin', sign_in_view, name='Signin'),
-    path('Player/<pk>/', UserView.as_view(), name='player')
+    path('<slug:slug>/', PlayerView.as_view(), name='player')
 ]
